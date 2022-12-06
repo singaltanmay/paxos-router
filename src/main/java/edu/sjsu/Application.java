be456cd23287770e38cadb8a21e08e257c62c85f
Tanmay Singal <tanmaysingal@icloud.com>
2022-12-05 17:08:34 -0800

    Added swagget UI

--- src/main/java/edu/sjsu/Application.java
+++ src/main/java/edu/sjsu/Application.java
@@ -6,13 +6,16 @@ package edu.sjsu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
 
-    public enum PAXOS_ROLES {PROPOSER, ACCEPTOR, LEARNER}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  public enum PAXOS_ROLES {PROPOSER, ACCEPTOR, LEARNER}
 
 }
